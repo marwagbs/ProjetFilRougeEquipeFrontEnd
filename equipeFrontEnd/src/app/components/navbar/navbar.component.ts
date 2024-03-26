02e819bb51842ddb05fbad322dcf3457e811d661
unknown <marwagbs@hotmail.com>
2024-03-26 11:39:26 +0100

    la squelette du projet

---
+++ equipeFrontEnd/src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,17 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { IMenuRoute } from './menu-route';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [RouterModule, CommonModule],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss'
+})
+export class NavbarComponent {
+ public menuRoutes:IMenuRoute[]=[
+  {path:'accueil',libelle:'Accueil'}
+ ]
+}
