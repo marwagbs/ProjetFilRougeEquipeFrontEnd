0bc39904522e7557a60b84a61b268306d520a697
Sarah <sarah.taleb-le-bris@hotmail.com>
2024-03-27 13:58:14 +0100

    commit pour entity tableres

--- equipeFrontEnd/src/app/app.routes.ts
+++ equipeFrontEnd/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { PageAccueilComponent } from './components/page-accueil/page-accueil.component';
 import { PageReservationsComponent } from './components/page-reservations/page-reservations.component';
+import { AccueilClientComponent } from './components/accueil-client/accueil-client.component';
 
 export const routes: Routes = [
     { path: 'accueil-employe', component: PageAccueilComponent },
     { path: 'reservation', component: PageReservationsComponent },
+    { path: 'accueil-clientele', component: AccueilClientComponent},
 ];
